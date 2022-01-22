a0679ba3b7c0c95ce1ce3207fb577ccb735d273c
WY <hey.weiyang@gmail.com>
2022-01-22 13:30:50 -0500

    Update notes

--- app.js
+++ app.js
@@ -28,6 +28,26 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// 函数式编程思想设计 middleware
+
+// app.use(b1)
+// app.use(a1, a2, a3)
+// app.use(b1) //next让下面接着执行
+/*
+打印
+b1
+a1
+a2
+a3
+b1
+不同use间的中间件[app.use(b1)和app.use(a1, a2, a3)] 和 同一个use内的中间件[app.use(a1, a2, a3)]，只要不用next，就都不能连续调用。
+先写的use，先被用；同use内，先写的中间件先被用--先写先运行，若不用next，后写的无法执行
+
+另外，因为重复使用app.use(b1)，浏览器会报错拒收：server不安全
+*/
+
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
